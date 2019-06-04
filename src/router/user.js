c4535acf6014c4a49779c7cc5c34fcbcb74393f3
zhangqi <jone09@outlook.com>
2019-06-04 15:50:58 +0800

    添加resModel

--- src/router/user.js
+++ src/router/user.js
@@ -1,16 +1,11 @@
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 const handleUserRouter = (req, res) => {
     if(req.method === 'POST' && req.path === '/api/user/login'){
-        return {
-            errorCode: 0,
-            message: '登录成功'
-        }
+        return new SuccessModel('登录成功')
     }
     
     if(req.method === 'GET' && req.path === '/api/user/checkLogin'){
-        return {
-            errorCode: -1,
-            message: '检验登录'
-        }
+        return new ErrorModel('登录失败')
     }
 }
 
