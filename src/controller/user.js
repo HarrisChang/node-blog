49c722f8f540e394439bbbd51be946946f994882
zhangqi <jone09@outlook.com>
2019-06-09 21:36:16 +0800

    添加密码加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,10 @@
 const { execSql, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 
 const login = (username, password) => {
     username = escape(username)
+    // 生成加密密码
+    password = genPassword(password)
     password = escape(password)
     let sql = `select username, realname from users where username=${username} and password=${password}`
     return execSql(sql).then(data => {
