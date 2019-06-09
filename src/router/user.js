de754100445a56871ea10bd2e88c6285600230f5
zhangqi <jone09@outlook.com>
2019-06-09 17:11:42 +0800

    删除测试代码

--- src/router/user.js
+++ src/router/user.js
@@ -2,9 +2,18 @@ const { login, checkLogin } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 const { setRedis } = require('../db/redis')
 
+// 统一的登录验证函数
+const loginCheck = req => {
+    if(!req.session.username){
+        return Promise.resolve(
+            new ErrorModel('登录信息已失效')
+        )
+    }
+}
+
 const handleUserRouter = (req, res) => {
-    if(req.method === 'GET' && req.path === '/api/user/login'){
-        const { username, password } = req.query
+    if(req.method === 'POST' && req.path === '/api/user/login'){
+        const { username, password } = req.body
         return login(username, password).then(data => {
             if(data.username){
                 // 操作cookie
@@ -19,30 +28,6 @@ const handleUserRouter = (req, res) => {
         })
     }
     
-    if(req.method === 'GET' && req.path === '/api/user/checkLogin'){
-        return checkLogin(req.query.username, req.query.password).then(result => {
-            if(result[0].username){
-                return new SuccessModel({
-                    data: result[0]
-                })
-            }else{
-                return new ErrorModel('登录信息已失效')
-            }
-        }).catch(err => {
-            return new ErrorModel('登录信息已失效')
-        })
-    }
-
-    if(req.method === 'GET' && req.path === '/api/user/login-test'){
-        if(req.session.username){
-            return Promise.resolve(
-                new SuccessModel()
-            )
-        }
-        return Promise.resolve(
-            new ErrorModel('登录信息已失效')
-        )
-    }
 }
 
 module.exports = handleUserRouter
